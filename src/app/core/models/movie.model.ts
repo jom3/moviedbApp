3f41b13cf7f15f8c43c50ad1a077a3404dfbd6bd
joams mogro gomez <joamsmg@gmail.com>
2023-07-28 16:22:38 -0400

    some change on movie model

--- src/app/core/models/movie.model.ts
+++ src/app/core/models/movie.model.ts
@@ -1,7 +1,7 @@
-export interface Movie {
+export interface MovieResponse {
     dates:        Dates;
     page:         number;
-    results:      Result[];
+    results:      MoviesData[];
     totalPages:   number;
     totalResults: number;
 }
@@ -11,7 +11,7 @@ export interface Dates {
     minimum: Date;
 }
 
-export interface Result {
+export interface MoviesData {
     adult:            boolean;
     backdropPath:     string;
     genreIDS:         number[];
