4db3a961991b7f8a0dc125f0705b08739ce79c51
joams mogro gomez <joamsmg@gmail.com>
2023-08-09 19:29:45 -0400

    adding some modules and some change on html

--- src/app/shared/components/search-serie-card/search-serie-card.component.ts
+++ src/app/shared/components/search-serie-card/search-serie-card.component.ts
@@ -1,21 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { environment } from 'src/environments/environment.development';
-import { Movie } from 'src/app/core/models/movie.model';
+import { Serie } from 'src/app/core/models/serie.model';
 import { RouterModule } from '@angular/router';
 
 @Component({
-  selector: 'app-movie-card',
+  selector: 'app-search-serie-card',
   standalone: true,
-  imports: [CommonModule,RouterModule],
-  templateUrl: './movie-card.component.html',
-  styleUrls: ['./movie-card.component.css']
+  imports: [CommonModule, RouterModule],
+  templateUrl: './search-serie-card.component.html',
+  styleUrls: ['./search-serie-card.component.css']
 })
-export class MovieCardComponent {
-
+export class SearchSerieCardComponent {
   frontImage:string = environment.front_image;
 
-  @Input() data!:Movie;
+  @Input() data!:Serie;
 
   getImage(path:string){
     return `${this.frontImage}/${path}`;
--- src/app/shared/components/serie-card/serie-card.component.ts
+++ src/app/shared/components/serie-card/serie-card.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { environment } from 'src/environments/environment.development';
+import { Serie } from 'src/app/core/models/serie.model';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-serie-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   templateUrl: './serie-card.component.html',
   styleUrls: ['./serie-card.component.css']
 })
 export class SerieCardComponent {
 
+  frontImage:string = environment.front_image;
+
+  @Input() data!:Serie;
+
+  getImage(path:string){
+    return `${this.frontImage}/${path}`;
+  }
 }
