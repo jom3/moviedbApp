551850b86c95b106135bd2fb3eef2d0c23fdda20
joams mogro gomez <joamsmg@gmail.com>
2023-08-09 19:28:32 -0400

    adding some principals routes

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 const routes: Routes = [
+  {
+    path:'',
+    component:MainPageComponent,
+  },
+  {
+    path:'search',
+    component:SearchPageComponent
+  },
   {
     path:'',
     loadChildren:()=>import('../peliculas/peliculas.module').then(m=>m.PeliculasModule)
