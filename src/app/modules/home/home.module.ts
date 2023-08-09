89dad1a7e0a8d6fe5e791901fce535bf26687ccd
joams mogro gomez <joamsmg@gmail.com>
2023-08-09 19:26:18 -0400

    some modules and standalone components added

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -6,6 +6,13 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { MovieCardComponent } from 'src/app/shared/components/movie-card/movie-card.component';
+import { SerieCardComponent } from 'src/app/shared/components/serie-card/serie-card.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { SearchMovieCardComponent } from 'src/app/shared/components/search-movie-card/search-movie-card.component';
+import { SearchSerieCardComponent } from 'src/app/shared/components/search-serie-card/search-serie-card.component';
 
 
 @NgModule({
@@ -15,10 +22,18 @@ import { FooterComponent } from './components/footer/footer.component';
        NotFoundPageComponent,
        NavbarComponent,
        FooterComponent,
+       MainPageComponent,
+       SearchPageComponent,
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    SerieCardComponent,
+    MovieCardComponent,
+    SearchMovieCardComponent,
+    SearchSerieCardComponent
   ]
 })
 export class HomeModule { }
