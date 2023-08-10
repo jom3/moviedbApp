cb19e85ca2bb369badd8c79f87d1d8d8f8d765eb
joams mogro gomez <joamsmg@gmail.com>
2023-08-10 00:32:51 -0400

    console log deleted

--- src/app/modules/home/components/navbar/navbar.component.ts
+++ src/app/modules/home/components/navbar/navbar.component.ts
@@ -14,6 +14,5 @@ export class NavbarComponent {
  ngOnInit(): void {
   //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
   //Add 'implements OnInit' to the class.
-  console.log(navigation)
  }
 }
