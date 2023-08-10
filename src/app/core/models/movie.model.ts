2eb1737f9e103d71d9abaaecd311b79b270e989e
joams mogro gomez <joamsmg@gmail.com>
2023-08-10 00:31:23 -0400

    adding optional type

--- src/app/core/models/movie.model.ts
+++ src/app/core/models/movie.model.ts
@@ -16,12 +16,12 @@ export interface Movie {
   backdrop_path: string;
   genre_ids: number[];
   id: number;
-  original_language: OriginalLanguage;
+  original_language?: OriginalLanguage;
   original_title: string;
   overview: string;
   popularity: number;
   poster_path: string;
-  release_date: Date;
+  release_date?: Date;
   title: string;
   video: boolean;
   vote_average: number;
