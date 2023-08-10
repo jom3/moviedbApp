28e265357f3ec303078d9455091ed820ebce4e80
joams mogro gomez <joamsmg@gmail.com>
2023-08-10 00:43:35 -0400

    comment added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   baseUrl:'https://api.themoviedb.org/3',
-  api_key:'cc92c7d960540569124abed0ae253d07',
+  api_key:'cc92c7d960540569124abed0ae253d07', //you must change it for your api key :)
   language:'en-US',
   front_image:'https://image.tmdb.org/t/p/w500',
   back_image:'https://image.tmdb.org/t/p/original'
