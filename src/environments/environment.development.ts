34ab0309bd349903d27b3407e9dfb84066addf7e
Joamsmg <joamsmg@gmail.com>
2023-08-10 00:36:06 -0400

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
-  baseUrl:'https://api.themoviedb.org/3',
-  api_key:'cc92c7d960540569124abed0ae253d07',
-  language:'en-US',
-  front_image:'https://image.tmdb.org/t/p/w500',
-  back_image:'https://image.tmdb.org/t/p/original'
+  baseUrl:'',
+  api_key:'',
+  language:'',
+  front_image:'',
+  back_image:''
 };
